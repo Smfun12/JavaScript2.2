0bcdf9bfaefcd933b9b7eef1fe24462e48e9ab2c
Smfun <happylife1202>
2021-01-30 14:14:12 +0200

    3rd commit

--- task3/main.js
+++ task3/main.js
@@ -1,51 +1,12 @@
 var LIST    =   $('.bl-list');
 var ITEM_TEMPLATE   =   $('.bl-row').html();
-     
+var node = $(".bl-row");
+var newNode = node.clone();     
             $(document).ready(function(){
-            $(".bl-plus").click(function(){
-            var counter = $(this).parent().find(".bl-label");
-             var minus = $(this).parent().find(".bl-minus");
-            var x = $(counter).text();
-             counter.fadeOut(250, function(){
-                        counter.fadeIn(250);
-                  $(counter).text(+x+1);
-                  });
-                  $(minus).attr('disabled',false);
-                  
-              if (x+1>1){
-                 
-             $(minus).css("opacity", 1);
-             $(minus).css("background-color", "#DA2E2E");
-             $(minus).css("cursor", "pointer");
-            }
-            
-  });
-   $(".bl-minus").click(function(){
-      var counter = $(this).parent().find(".bl-label");
-      var x = $(counter).text();
-            if (x>1){
-                  counter.fadeOut(250, function(){
-                        counter.fadeIn(250);
-                  
-                  $(counter).text(x-1);
-                  });
-                  $(this).attr('disabled',false);
-                  
-            }
-            x = $(counter).text();
-            if(x==2){
-            $(this).css("background-color", "#E13E3E");
-               $(this).css("opacity", 0.5);
-                $(this).attr('disabled',true);
-               $(this).css("cursor", "context-menu");
-            }  
-   });
-
    $(".placeholder-button").click(function(){
      var text = $(".placeholder input").val();
      if (text != ''){
-           var node = $(".bl-row");
-           var newNode = node.clone();
+           
      newNode.find('.bl-product').text(text);
      newNode.find('.bl-label').text(1);
      newNode.find('.bl-minus')
@@ -60,16 +21,51 @@ var ITEM_TEMPLATE   =   $('.bl-row').html();
             ("<div class='bl-row'>"+newNode.html())
             .append('</div>');}
               });
-              
-    $(".delete-button").click(function(){
-     $(this).parent().
-     parent().find('hr').remove();
-     $(this).parent().parent()
-     .css("display","none");
-     
-   });
 });
 
-function increment(){
-      console.log('hello');
+function decrement(element){
+      var counter = $(element).parent().find(".bl-label");
+      var x = $(counter).text();
+            if (x>1){
+                  counter.fadeOut(250, function(){
+                        counter.fadeIn(250);
+                  
+                  $(counter).text(x-1);
+                  });
+                  $(element).attr('disabled',false);
+                  
+            }
+            x = $(counter).text();
+            if(x==2){
+            $(element).css("background-color", "#E13E3E");
+               $(element).css("opacity", 0.5);
+                $(element).attr('disabled',true);
+               $(element).css("cursor", "context-menu");
+            }
+}
+function increment(element){
+      var counter = $(element).parent().find(".bl-label");
+             var minus = $(element).parent().find(".bl-minus");
+            var x = $(counter).text();
+             counter.fadeOut(250, function(){
+                        counter.fadeIn(250);
+                  $(counter).text(+x+1);
+                  });
+                  $(minus).attr('disabled',false);
+                  
+              if (x+1>1){
+                 
+             $(minus).css("opacity", 1);
+             $(minus).css("background-color", "#DA2E2E");
+             $(minus).css("cursor", "pointer");
+            }
+      var text = $(element).parent().find(".bl-product").text();
+            
+}
+
+function deleteProduct(element){
+     $(element).parent().
+     parent().parent().find('hr').remove();
+     $(element).parent().parent()
+     .remove();
 }
